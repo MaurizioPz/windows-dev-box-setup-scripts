c217d4c50f580f0b8840253da77b86284a5656a9
Maurizio <mauriziopz@gmail.com>
2020-01-22 09:27:58 +0100

    Update dev_app_desktop_.NET.ps1

--- dev_app_desktop_.NET.ps1
+++ dev_app_desktop_.NET.ps1
@@ -26,23 +26,17 @@ executeScript "SystemConfiguration.ps1";
 executeScript "FileExplorerSettings.ps1";
 executeScript "RemoveDefaultApps.ps1";
 executeScript "CommonDevTools.ps1";
+executeScript "Browsers.ps1";
 
 #--- Tools ---
-#--- Installing VS and VS Code with Git
-# See this for install args: https://chocolatey.org/packages/VisualStudio2017Community
-# https://docs.microsoft.com/en-us/visualstudio/install/workload-component-id-vs-community
-# https://docs.microsoft.com/en-us/visualstudio/install/use-command-line-parameters-to-install-visual-studio#list-of-workload-ids-and-component-ids
-# visualstudio2017community
-# visualstudio2017professional
-# visualstudio2017enterprise
 
-choco install -y visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+choco install -y visualstudio2019community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
 Update-SessionEnvironment #refreshing env due to Git install
 
-#--- UWP Workload and installing Windows Template Studio ---
-choco install -y visualstudio2017-workload-azure
-choco install -y visualstudio2017-workload-manageddesktop
-
+#--- Web Workload ---
+choco install visualstudio2019-workload-netweb
+#--- Profiling
+choco install -y dotnetdeveloperbundle
 #--- reenabling critial items ---
 Enable-UAC
 Enable-MicrosoftUpdate
