180ef11215a98d37231340c5702bff05f00216ea
Maurizio <mauriziopz@gmail.com>
2020-02-17 16:55:43 +0100

    Update CommonDevTools.ps1

--- scripts/CommonDevTools.ps1
+++ scripts/CommonDevTools.ps1
@@ -2,6 +2,9 @@
 # tools we expect devs across many scenarios will want
 choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+choco install -y notepadplusplus
+choco install -y git-fork
+choco install -y winmerge
 choco install -y python
 choco install -y 7zip.install
 choco install -y sysinternals
